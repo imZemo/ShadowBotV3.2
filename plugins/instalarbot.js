a3f03f553d39fe491624dfe8683a5b95ad9ba6db
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-24 18:35:58 -0500

    ©ShadowBot

--- plugins/instalarbot.js
+++ plugins/instalarbot.js
@@ -11,23 +11,23 @@ let handler = async m => m.reply(`
 ➤ 2 teléfonos o un 1 teléfono y una PC (es temporal - solo para escanear un código QR)
 
 💥 𝐑𝐄𝐏𝐎𝐒𝐈𝐓𝐎𝐑𝐈𝐎 𝐃𝐄𝐋 𝐁𝐎𝐓 💥
-➤ https://github.com/BrunoSobrino/ShadowBotV3
+➤ https://github.com/BrunoSobrino/ShadowBotV3.2
 
 💥 𝐀𝐑𝐄𝐀 𝐃𝐄 𝐃𝐄𝐒𝐂𝐀𝐑𝐆𝐀𝐒 💥
 ➤ Termux: https://f-droid.org/en/packages/com.termux/
-➤ WhatsApp recomendado: https://download853.mediafire.com/xawpdplr7wvg/u133oxjqtiry52t/%E2%9E%A2+SOLITARIO+BUSINESS+%E2%9E%A2_2.21.19.21.apk
+➤ WhatsApp recomendado: https://www.mediafire.com/file/gers3gbbubpshji/%C3%8A%C2%99%C3%A1%C2%B4%C2%9Cs%C3%8D%C2%A8%C3%89%C2%AA%C3%8D%C2%A7%C3%89%C2%B4%C3%A1%C2%B7%C2%A8%C3%A1%C2%B4%C2%87%C3%8D%C2%A3s%C3%A1%C2%B7%C2%A1s%C3%8D%C2%A6+%C3%A2%C2%A9%C2%945_Secundario.apk/file
    
 💥 𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐃𝐄 𝐈𝐍𝐒𝐓𝐀𝐋𝐀𝐂𝐈𝐎𝐍 𝐕𝐈𝐀 𝐆𝐈𝐓𝐇𝐔𝐁 💥
-> termux-setup-storage
+➤ termux-setup-storage
 ➤ apt update && apt upgrade -y -y
 ➤ apt install git -y
 ➤ apt install nodejs -y
 ➤ apt install ffmpeg -y
 ➤ apt install imagemagick -y
-➤ git clone https://github.com/BrunoSobrino/ShadowBotV3
-➤ cd ShadowBotV3
+➤ git clone https://github.com/BrunoSobrino/ShadowBotV3.2
+➤ cd ShadowBotV3.2
 ➤ npm install
-➤ npm install -g npm@8.5.4
+➤ npm install -g npm@8.7.0
 ➤ npm update
 ➤ npm start
 ° Escaneas el código QR tienes 30 segundos para hacerlo
@@ -35,7 +35,7 @@ let handler = async m => m.reply(`
 💥 INFO EXTRA 💥
 ➤ Se sugiere leer el archivo README.md 
 ➤ Bot no compartible con WhatsApp web multi device 
-➤ Para habilitar el /añadir y el /sacar usa #enable autoread
+➤ Para habilitar el /añadir y el /sacar usa /enable autoread
 - Puede ocasionar que el numero se vaya a soporte 
 ➤ PayPal para apoyo voluntario: https://www.paypal.me/TheShadowBrokers133
 ➤ Numero de ayuda durante la instalacion: wa.me/5219992095479 (No Bot, solo ayuda durante la instalación)
@@ -45,5 +45,4 @@ let handler = async m => m.reply(`
 handler.help = ['instalarbot']
 handler.tags = ['info']
 handler.command = /^instalarbot$/i
-
 module.exports = handler
