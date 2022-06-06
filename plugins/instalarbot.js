b26857b83f224a64fb258145219437fd5ffd0e09
imZemo <106836635+imZemo@users.noreply.github.com>
2022-06-06 02:12:06 -0500

    Update instalarbot.js

--- plugins/instalarbot.js
+++ plugins/instalarbot.js
@@ -1,46 +1,5 @@
 let handler = async m => m.reply(`
-*◄┢┅͜͡✇⟬↯ື ►ஜ۩💥۩ஜ◄ ↯ື⟭✇͜͡┅┧►*
-
-*VIDEO TUTORIAL: Proximamente en https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA?sub_confirmation=1* 
-
-💥 𝐑𝐄𝐐𝐔𝐈𝐒𝐈𝐓𝐎𝐒 𝐏𝐀𝐑𝐀 𝐋𝐀 𝐈𝐍𝐒𝐓𝐀𝐋𝐀𝐂𝐈𝐎𝐍 💥
-➤ Termux  
-➤ 1 GB de memoria en el teléfono
-➤ Aconsejable un WhatsApp secundario
-➤ Aconsejable un numero secundario
-➤ 2 teléfonos o un 1 teléfono y una PC (es temporal - solo para escanear un código QR)
-
-💥 𝐑𝐄𝐏𝐎𝐒𝐈𝐓𝐎𝐑𝐈𝐎 𝐃𝐄𝐋 𝐁𝐎𝐓 💥
-➤ https://github.com/BrunoSobrino/ShadowBotV3.2
-
-💥 𝐀𝐑𝐄𝐀 𝐃𝐄 𝐃𝐄𝐒𝐂𝐀𝐑𝐆𝐀𝐒 💥
-➤ Termux: https://f-droid.org/en/packages/com.termux/
-➤ WhatsApp recomendado: https://www.mediafire.com/file/gers3gbbubpshji/%C3%8A%C2%99%C3%A1%C2%B4%C2%9Cs%C3%8D%C2%A8%C3%89%C2%AA%C3%8D%C2%A7%C3%89%C2%B4%C3%A1%C2%B7%C2%A8%C3%A1%C2%B4%C2%87%C3%8D%C2%A3s%C3%A1%C2%B7%C2%A1s%C3%8D%C2%A6+%C3%A2%C2%A9%C2%945_Secundario.apk/file
-   
-💥 𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐃𝐄 𝐈𝐍𝐒𝐓𝐀𝐋𝐀𝐂𝐈𝐎𝐍 𝐕𝐈𝐀 𝐆𝐈𝐓𝐇𝐔𝐁 💥
-➤ termux-setup-storage
-➤ apt update && apt upgrade -y -y
-➤ apt install git -y
-➤ apt install nodejs -y
-➤ apt install ffmpeg -y
-➤ apt install imagemagick -y
-➤ git clone https://github.com/BrunoSobrino/ShadowBotV3.2
-➤ cd ShadowBotV3.2
-➤ npm install
-➤ npm install -g npm@8.7.0
-➤ npm update
-➤ npm start
-° Escaneas el código QR tienes 30 segundos para hacerlo
-
-💥 INFO EXTRA 💥
-➤ Se sugiere leer el archivo README.md 
-➤ Bot no compartible con WhatsApp web multi device 
-➤ Para habilitar el /añadir y el /sacar usa /enable autoread
-- Puede ocasionar que el numero se vaya a soporte 
-➤ PayPal para apoyo voluntario: https://www.paypal.me/TheShadowBrokers133
-➤ Numero de ayuda durante la instalacion: wa.me/5219992095479 (No Bot, solo ayuda durante la instalación)
-
-*◄┢┅͜͡✇⟬↯ື ►ஜ۩💥۩ஜ◄ ↯ື⟭✇͜͡┅┧►*
+puto el que lo lea desputo el que diga que es puto :v
 `.trim()) 
 handler.help = ['instalarbot']
 handler.tags = ['info']
