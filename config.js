7f5548f6c564b2af60b9a43749f0c41037a27ded
imZemo <106836635+imZemo@users.noreply.github.com>
2022-06-06 01:58:35 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ' , 'https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs' , 'https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F' , 'https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb'] // No tiene utilidad 
+global.linkGC = ['https://chat.whatsapp.com/Bt1sRtyLup70KIpMWHI8LD' , 'https://chat.whatsapp.com/Bt1sRtyLup70KIpMWHI8LD' , 'https://chat.whatsapp.com/Bt1sRtyLup70KIpMWHI8LD' , 'https://chat.whatsapp.com/Bt1sRtyLup70KIpMWHI8LD' , 'https://chat.whatsapp.com/Bt1sRtyLup70KIpMWHI8LD'] // No tiene utilidad 
 global.channelYT = ['https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'] // No tiene utilidad
-global.owner = ['17722386341'] //Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
-global.mods = ['5219991402134'] //No tiene utilidad
-global.prems = ['5219996125657' , '5219992095479', '972557048356'] //Podra usar algunos comandos del apartado de propietario
+global.owner = ['51917611323' , '51921909260' , '51933462225'] //Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
+global.mods = ['51917611323'] //No tiene utilidad
+global.prems = ['51917611323' , '51921909260' , '51933462225'] //Podra usar algunos comandos del apartado de propietario
 
 //Lo siguiente no tiene nada que ver con el funcionamiento del Bot:
 //Nota: Puedes contactarme si necesitas ayuda con algo al +5219996125657 (este es mi numero personal/real bloqueo personas que no sean temas de la instalación)
@@ -56,10 +56,10 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = 'The Shadow Brokers - Bot'
+global.author = '𝐔𝐧 𝐒𝐢𝐦𝐩𝐥𝐞 𝐁𝐨𝐭𝐬𝐢𝐭𝐨 𓃵'
 
 
-global.wm = '©The Shadow Brokers - Bot'
+global.wm = '©𝐔𝐧 𝐒𝐢𝐦𝐩𝐥𝐞 𝐁𝐨𝐭𝐬𝐢𝐭𝐨 𓃵'
 global.wait = '*Espere un momento..*'
 
 
